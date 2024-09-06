a91fc51a975937ac6a710d54ea5bd9431f0e0959
justkira <shighgamerhg@gmail.com>
2024-09-06 16:23:57 +0300

    chore: add bevy & bevy_ecs_tilemap with basic camera in scene

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle {
+        projection: OrthographicProjection {
+            near: -1000.0,
+            far: 1000.0,
+            ..Default::default()
+        },
+        ..Default::default()
+    });
 }
